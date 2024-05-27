2869091c45280d82b085ba2e755b50853ac76f18
dqmis <dom.seputis@gmail.com>
2024-05-27 21:47:32 +0200

    Updated files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from generators.graph import export_data as export_graph_data
+from generators.set_cards import export_data as export_set_data
 from generators.sudoku import export_data as export_sudoku_data
 
 N_SAMPLES = 200
@@ -14,3 +15,8 @@ if __name__ == "__main__":
     print("Exporting Graph data...")
     export_graph_data(ROOT_DIR, N_SAMPLES, fill_in=False)
     export_graph_data(ROOT_DIR, N_SAMPLES, fill_in=True)
+
+    print("Exporting SET data...")
+    export_set_data(ROOT_DIR, N_SAMPLES)
+
+    print("Data exported successfully!")
